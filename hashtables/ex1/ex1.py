6a70d8523aea26af40932804ec13cda28ee4d7d6
TheTrabin <beyondtears@gmail.com>
2020-11-08 20:21:07 -0800

    Done. Answered Questions.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,10 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
-
+    lookup = {weight: i for (i, weight) in enumerate(weights)}
+    for (i, weight) in enumerate(weights):
+        needed_weight = limit - weight
+        if needed_weight in lookup:
+            return (max(i, lookup[needed_weight]), min(i, lookup[needed_weight]))
+            
     return None
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,8 +3,9 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
-
-    return result
+    negatives = {num: True for num in a if num < 0}
+    return [num for num in a if num > 0 and -num in negatives]
+    # return result
 
 
 if __name__ == "__main__":
